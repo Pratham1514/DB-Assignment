cea581f1c12c74dcb4334a9c6621b5e404c30004
Pratham Gite <99612533+Pratham1514@users.noreply.github.com>
2024-03-11 00:02:58 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,41 @@
+CREATE TABLE product (
+  id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  desc TEXT,
+  SKU VARCHAR(50),
+  category_id INT,
+  inventory_id INT,
+  price DECIMAL,
+  discount_id INT,
+  created_at TIMESTAMP,
+  modified_at TIMESTAMP,
+  deleted_at TIMESTAMP
+);
 
+CREATE TABLE product_category (
+  id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  desc TEXT,
+  created_at TIMESTAMP,
+  modified_at TIMESTAMP,
+  deleted_at TIMESTAMP
+);
+
+CREATE TABLE product_inventory (
+  id INT PRIMARY KEY,
+  quantity INT,
+  created_at TIMESTAMP,
+  modified_at TIMESTAMP,
+  deleted_at TIMESTAMP
+);
+
+CREATE TABLE product (
+  id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  desc TEXT,
+  discount_percent DECIMAL,
+  active BOOLEAN,
+  created_at TIMESTAMP,
+  modified_at TIMESTAMP,
+  deleted_at TIMESTAMP
+);
